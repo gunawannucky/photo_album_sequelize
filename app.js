122dc5f45e500f307d72252ef4e70c27967c75a2
hacktivate <gunawannucky@gmail.com>
2019-08-30 09:37:18 +0700

    app listen 2

--- app.js
+++ app.js
@@ -287,8 +287,6 @@ app.route('/photo/:id/add')
         }
     });
 
-
-
-app.listen(process.env.PORT || 3000, function () {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(port, function() {
+    console.log(`server at ${port}`)
+})
